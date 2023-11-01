cca7d0c8d9a05a2d928139f20b48b287c202fed8
AHUYNH00 <AHUYNH00@UW.EDU>
2023-10-31 17:51:18 -0700

    populated topic overview"

--- app/src/main/java/edu/uw/ischool/ahuynh00/quizdroid/MainActivity.kt
+++ app/src/main/java/edu/uw/ischool/ahuynh00/quizdroid/MainActivity.kt
@@ -6,6 +6,73 @@ import android.os.Bundle
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import android.widget.TextView
+
+data class Quiz(val description: String, val questions: Array<Question>) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Quiz
+
+        if (description != other.description) return false
+        if (!questions.contentEquals(other.questions)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = description.hashCode()
+        result = 31 * result + questions.contentHashCode()
+        return result
+    }
+}
+
+data class Question(val prompt: String, val options: Array<String>, val answer: String) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Question
+
+        if (prompt != other.prompt) return false
+        if (!options.contentEquals(other.options)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = prompt.hashCode()
+        result = 31 * result + options.contentHashCode()
+        return result
+    }
+}
+
+val mathQuestions = arrayOf(
+    Question("What is 2 + 2?", arrayOf("3", "4", "5", "6"),
+        "4" ),
+    Question("If i have 3 apples and eat one, how many apples do i have left?", arrayOf("0", "1", "2", "3"),
+        "2")
+)
+
+val physicsQuestions = arrayOf(
+    Question("What is the gravitational acceleration constant in m/s^2?", arrayOf("3.14", "5.67", "7.83", "9.81"),
+        "9.81"),
+    Question("How many seconds are there in a minute?", arrayOf("15", "30", "45", "60"),
+        "60")
+)
+
+val marvel_super_heroesQuestions = arrayOf(
+    Question("How many infinite stones are there?", arrayOf("6", "7", "8", "9"), "6"),
+    Question("What does S.H.I.E.L.D. stand for?", arrayOf("Security Hope Insurance Empowerment Life Devoted",
+        "Secure Hostile Intelligence Expert Life Division",
+        "Superhuman Homeland Intelligence Enforcement and Logistics Division",
+        "Security Homeland Intervention Enforcement and Logistics Division"), "Security Homeland Intervention Enforcement and Logistics Division")
+)
+
+val mathQuiz = Quiz("Mathematics is an area of knowledge that includes the topics of numbers, formulas and related structures, shapes and the spaces in which they are contained", mathQuestions)
+val physicsQuiz = Quiz("Physics is the natural science of matter, involving the study of matter, its fundamental constituents, its motion and behavior through space and time", physicsQuestions)
+val marvel_super_heroesQuiz = Quiz("Marvel Comics is an American comic book publisher and the property of The Walt Disney Company since December 31, 2009", marvel_super_heroesQuestions)
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,9 +86,9 @@ class MainActivity : AppCompatActivity() {
         arrayAdapter = ArrayAdapter(this, android.R.layout.activity_list_item, android.R.id.text1, quizTopics)
         quizListView.adapter = arrayAdapter
         quizListView.onItemClickListener = AdapterView.OnItemClickListener{ _, _, position, _ ->
-            val intent = Intent(this, TopicOverview::class.java)
-            intent.putExtra("selected topic", quizTopics[position])
-            startActivity(intent)
+            val quizOverviewIntent = Intent(this, TopicOverview::class.java)
+            quizOverviewIntent.putExtra("selected topic", quizTopics[position])
+            startActivity(quizOverviewIntent)
         }
     }
 }
--- app/src/main/java/edu/uw/ischool/ahuynh00/quizdroid/TopicOverview.kt
+++ app/src/main/java/edu/uw/ischool/ahuynh00/quizdroid/TopicOverview.kt
@@ -2,10 +2,38 @@ package edu.uw.ischool.ahuynh00.quizdroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+
+
+
 
 class TopicOverview : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_topic_overview)
+        val quizTopic = intent.getStringExtra("selected topic")
+
+        val description = findViewById<TextView>(R.id.topic_description)
+        val totalQuestions = findViewById<TextView>(R.id.total_questions)
+        val beginBtn = findViewById<Button>(R.id.begin_button)
+
+        when (quizTopic) { //another way to do this?
+            "Math" -> {
+                description.text = mathQuiz.description
+                totalQuestions.text = "${mathQuiz.questions.size.toString()} questions"
+            } "Physics" -> {
+                description.text = physicsQuiz.description;
+                totalQuestions.text = "${physicsQuiz.questions.size.toString()} questions";
+            } "Marvel Super Heroes" -> {
+                description.text = marvel_super_heroesQuiz.description
+                totalQuestions.text = "${marvel_super_heroesQuiz.questions.size.toString()} questions";
+            } else -> description.text = "Quiz not found"
+        }
+        beginBtn.setOnClickListener {
+            // set intent and adapter stuff here
+        }
+
     }
 }
